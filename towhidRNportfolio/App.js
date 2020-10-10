a88c058e19ae8492c73b94dbd2081472c19a57b3
TowhidAust <towhidaustcse33@gmail.com>
2020-10-11 00:50:22 +0600

    initial commit with react native firebase setup

---
+++ towhidRNportfolio/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button, Text } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Button title="This is a button" ></Button>
+        <Text>Hi this is towhid react portfolio</Text>
+      </>
+    )
+  }
+}
+
